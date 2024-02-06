93269441ad0785b8350b0998051f121f15ab57ff
Irvin Gomes <irvin.r.gomes@gmail.com>
2024-02-05 20:54:06 -0400

    creating separated reducers and using combineReducers

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,8 +1,14 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
+//Actions
 const BUY_CAKE = "BUY_CAKE";
 const BAKE_CAKE = "BAKE_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+//Action creators
+//CAKES
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -16,13 +22,71 @@ function bakeCake() {
     info: "Second redux action",
   };
 }
+//ICECREAM
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    infor: "Third redux action",
+  };
+}
 
 // (prevState, action) => newState
 const initialState = {
   numOfCakes: 10,
+  numOfIceCream: 20,
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case BAKE_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + 1,
+      };
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// Dispatches
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated State", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(bakeCake());
+store.dispatch(bakeCake());
+store.dispatch(bakeCake());
+store.dispatch(bakeCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
+
+/* Splited */
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCream: 10,
+};
+
+const CakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -39,16 +103,52 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
-console.log("Initial State", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated State", store.getState())
+const IceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamStore = createStore(IceCreamReducer);
+console.log("Initial IceCream State", iceCreamStore.getState());
+const iceCreaUnsubscribe = iceCreamStore.subscribe(() =>
+  console.log("Updated State", iceCreamStore.getState())
 );
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(bakeCake());
-store.dispatch(bakeCake());
-store.dispatch(bakeCake());
-store.dispatch(bakeCake());
-store.dispatch(buyCake());
-unsubscribe();
+const cakeStore = createStore(CakeReducer);
+console.log("Initial Cake State", cakeStore.getState());
+const cakeUnsubscribe = cakeStore.subscribe(() =>
+  console.log("Updated State", cakeStore.getState())
+);
+
+iceCreamStore.dispatch(buyIceCream());
+cakeStore.dispatch(buyCake());
+iceCreaUnsubscribe();
+cakeUnsubscribe();
+
+/* Combined stores */
+
+const rootReducer = combineReducers({
+  cake: CakeReducer,
+  iceCream: IceCreamReducer,
+});
+
+const combinedStore = createStore(rootReducer);
+console.log("Initial Combined State", combinedStore.getState());
+const combinedUnsibscribe = combinedStore.subscribe(() => {
+  console.log("Updated Combined State", combinedStore.getState());
+});
+combinedStore.dispatch(buyCake());
+combinedStore.dispatch(buyCake());
+combinedStore.dispatch(bakeCake());
+combinedStore.dispatch(buyIceCream());
+combinedStore.dispatch(buyIceCream());
+combinedUnsibscribe();
+
+console.log("Cakes:", combinedStore.getState().cake.numOfCakes);
+console.log("IceCreams:", combinedStore.getState().iceCream.numOfIceCream);
